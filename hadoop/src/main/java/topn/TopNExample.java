762288777d7fd2e06afe58db33e2ba13d46aa438
ChenLong828 <chenlong828@gmail.com>
2013-09-18 18:52:05 +0800

    重构wordcount，并且增加topn的演示示例

---
+++ hadoop/src/main/java/topn/TopNExample.java
@@ -0,0 +1,9 @@
+package topn;
+
+/**
+ * User: ChenLong
+ * Created Date: 9/18/13 6:49 下午
+ * Description:
+ */
+public class TopNExample {
+}
---
+++ hadoop/src/main/java/wordcount/Map.java
@@ -0,0 +1,36 @@
+package wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * User: ChenLong
+ * Created Date: 9/18/13 6:50 下午
+ * Description:
+ */
+public class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable>
+{
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output,
+                    Reporter reporter) throws IOException
+    {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens())
+        {
+            word.set(tokenizer.nextToken());
+            output.collect(word, one);
+        }
+    }
+}
